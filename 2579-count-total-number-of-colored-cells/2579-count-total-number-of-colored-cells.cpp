a715c106418ae6dc32cc7aeeefb57a0a82e10db0
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-03-04 21:37:25 +0530

    Time: 3 ms (66.67%), Space: 5.9 MB (100.00%) - LeetHub

---
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        if(n == 1)
+            return 1;
+        long long ans = 1;
+        for(int i=1; i<=n; i++)
+            ans += (i-1)*4;
+        
+        return ans;
+    }
+};
