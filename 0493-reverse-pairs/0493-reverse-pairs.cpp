5cbf3ba24973087ca7e62f060c162604ccf627d1
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-03-23 21:08:37 +0530

    Time: 221 ms (96.37%), Space: 46.9 MB (82.74%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,26 +1,12 @@
 class Solution {
 private:
-    int binS(vector<int>& nums, int low, int high, int target){
-        int i = low;
-        int j = high;
-        int mid = -1;
-        while(i <= j)
-        {
-            mid = i + (j-i)/2;
-            if((long long)nums[mid] <= 2*(long long)target)
-                i = mid+1;
-            else
-                j = mid-1;
-        }
-
-        return i;
-    }
-
     void mergeN(vector<int>& nums, int low, int mid, int high, int& ans, vector<int>& temp){
-        for(int j=mid+1; j<=high; j++)
+        int p = mid+1;
+        for(int i=low; i<=mid; i++)
         {
-            int pos = binS(nums, low, mid, nums[j]);
-            ans = ans + (mid-pos+1);
+            while(p<=high && nums[i]>2LL*nums[p])
+                p++;
+            ans = ans + (p-(mid+1));
         }
 
         int i = low;
