b7e8c9d4ca7cb3ba728540ca160e1f4cb403e6ad
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-03-25 17:56:33 +0530

    Time: 3 ms (83.63%), Space: 10.7 MB (79.84%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,6 +11,37 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // Time Complexity - O(n) Single traversal solution.
+        
+        // taking this instead of directly returning head, to avoid the edge case of n == len(Linked List)
+        ListNode *start = new ListNode();
+        start->next = head;         
+        ListNode *slow = start;
+        ListNode *fast = start;
+        
+        // Creating distance 'n' between the 2 points
+        for(int i=1; i<=n; i++)
+            fast = fast->next;
+        
+        // Once we reach 'n' distance between the 2 pointers, we will only move 1-1 step till end.
+        while(fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        // Deleting 'slow->next' node
+        slow->next = slow->next->next;
+        
+        return start->next;
+    }
+};
+
+
+/*
+    My Solution - TC-O(2n)
+    
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
         int len = 0;
         ListNode *temp = head;
         while(temp)
@@ -37,4 +68,4 @@ public:
 
         return head;
     }
-};
+*/
