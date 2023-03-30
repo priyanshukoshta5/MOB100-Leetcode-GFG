134c954af7cda2ac382302189e3b0d9629cb5856
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-03-30 16:35:23 +0530

    Time: 113 ms (76.61%), Space: 49.2 MB (96.58%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    bool isPalindrome(string& s){
+        for(int i=0, j=s.length()-1; i<j; i++, j--)
+            if(s[i] != s[j])
+                return false;
+        return true;
+    }
+
+    void solve(string& s, int ind, vector<string>& temp, vector<vector<string>>& ans){
+        if(ind == s.length())
+        {
+            ans.push_back(temp);
+            return;
+        }
+
+        string t;
+        for(int i=ind; i<s.length(); i++)
+        {
+            t.push_back(s[i]);
+            if(isPalindrome(t))
+            {
+                temp.push_back(t);
+                solve(s, i+1, temp, ans);
+                temp.pop_back();
+            }
+        }
+    }
+
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> temp;
+        solve(s, 0, temp, ans);
+        return ans;
+    }
+};
