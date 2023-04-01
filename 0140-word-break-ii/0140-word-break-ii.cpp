d863d6d3ab4eb15400a08fdf3a653c51c1ea39b9
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-04-01 14:31:28 +0530

    Time: 0 ms (100.00%), Space: 7 MB (95.39%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    void solve(int n, int pos, string& s, string temp, unordered_map<string,bool>& dict, vector<string>& ans){
+        if(pos == n)
+        {
+            temp.pop_back();
+            ans.push_back(temp);
+            return;
+        }
+
+        string word;
+        for(int i=pos; i<n; i++)
+        {
+            word.push_back(s[i]);
+            if(dict.find(word)!= dict.end())
+                solve(n, i+1, s, temp+word+" ", dict, ans);
+        }
+    }
+
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_map<string, bool> dict;
+        for(string s: wordDict)
+            dict[s] = true;
+        vector<string> ans;
+        solve(s.length(), 0, s, "", dict, ans);
+
+        return ans;
+    }
+};
