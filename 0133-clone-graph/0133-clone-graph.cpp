391721c0b90b128ea80516a43c71b3ff2043cc17
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-04-08 14:55:24 +0530

    Time: 7 ms (61.34%), Space: 8.9 MB (34.83%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+// Thanks NeetCode.
+
+class Solution {
+private:
+    Node* clone(Node* node, unordered_map<Node*, Node*>& copies){
+        if(node == nullptr)
+            return node;
+
+        // If the node is already present in the hashmap, we will use it
+        if(copies.find(node) != copies.end())
+            return copies[node];
+
+        // Creating a dummy/copy node
+        Node* newNode = new Node(node->val);
+        copies[node] = newNode;
+
+        // Marking all neighbors
+        for(auto it: node->neighbors)
+            newNode->neighbors.push_back(clone(it, copies));
+
+        return newNode;
+    }
+
+public:
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*, Node*> copies; // to store {original,copy}
+        return clone(node, copies);;
+    }
+};
