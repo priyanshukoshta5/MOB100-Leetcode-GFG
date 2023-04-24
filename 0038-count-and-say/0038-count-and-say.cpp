8c2989e6758bc36a689dbf5c0e4983f52cdf5c65
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-04-24 17:16:36 +0530

    Time: 56 ms (11.90%), Space: 121.2 MB (14.25%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    string recur(string str, int n){
-        if(n == 0)
-            return str;
-        
-        string temp = "";
-        int cnt = 0;
-        char ch = ' ';
-        for(char c: str)
-        {
-            if(c == ch)
-                cnt++;
-            else
-            {
-                if(cnt != 0)
-                {
-                    temp = temp + to_string(cnt);
-                    temp.push_back(ch);
-                }
-                ch = c;
-                cnt = 1;
-            }
-        }
-        temp = temp + to_string(cnt);
-        temp.push_back(ch);
-        
-        return recur(temp, n-1); 
-    }
-    
     string countAndSay(int n) {
-        return recur("1", n-1);
+        string ans = "1";
+        for(int i=2; i<=n; i++)
+        {
+            string temp = "";
+            int cnt = 0;
+            char ch = ' ';
+            for(char c: ans)
+            {
+                if(c == ch)
+                    cnt++;
+                else
+                {
+                    if(cnt != 0)
+                    {
+                        temp = temp + to_string(cnt);
+                        temp.push_back(ch);
+                    }
+                    ch = c;
+                    cnt = 1;
+                }
+            }
+            temp = temp + to_string(cnt);
+            temp.push_back(ch);
+            ans = temp;
+        }
+        return ans;
     }
 };
