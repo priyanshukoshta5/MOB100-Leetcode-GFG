cd80297d071aa4139ca1d8cf14f6864ab9aa6153
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-06-16 22:28:56 +0530

    Time: 3 ms (99.43%), Space: 6.5 MB (93.90%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,27 +1,26 @@
 class Solution {
-private:
-    int solve(string& word1, int i, string& word2, int j, vector<vector<int>> &dp){
-        if(i < 0)
-            return j+1;
-        if(j < 0)
-            return i+1;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        if(word1[i] == word2[j])
-            return dp[i][j] = solve(word1, i-1, word2, j-1, dp);
-        
-        return dp[i][j] = 1 + min(solve(word1, i-1, word2, j, dp),
-                                  min(solve(word1, i, word2, j-1, dp),
-                                      solve(word1, i-1, word2, j-1, dp)));
-    }
-    
 public:
     int minDistance(string word1, string word2) {
         int n = word1.length();
         int m = word2.length();
-        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
-        return solve(word1, n-1, word2, m-1, dp);
+        
+        vector<int> prev(m+1, 0), cur(m+1, 0);
+        for(int j = 0; j <= m; j++)
+            prev[j] = j;
+        
+        for(int i = 1; i <= n; i++)
+        {
+            cur[0] = i; //  Base Case
+            for(int j = 1; j <= m; j++)
+            {
+                if(word1[i-1] == word2[j-1])
+                    cur[j] = prev[j-1];
+                else
+                    cur[j] = 1 + min(prev[j], min(cur[j-1], prev[j-1]));
+            }
+            prev = cur;
+        }
+        
+        return prev[m];
     }
 };
