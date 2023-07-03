198a91f5c21259cf6015086a1e3cd93d67e0b64f
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-07-03 16:33:19 +0530

    Added solution - LeetHub

--- Palindromic Partitioning - GFG/palindromic-partitioning.cpp
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -7,39 +7,36 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-bool isPalindrome(string& s){
-    for(int i = 0, j = s.length()-1; i < j; i++, j--){
-        if(s[i] != s[j])
+bool isPalindrome(string& s, int i, int j){
+    while(i < j)
+    {
+        if(s[i] != s[j])    
             return false;
+        i++;
+        j--;
     }
     return true;
 }
 
 class Solution{
-private:
-    int solve(int i, string& str, vector<int> &dp){
-        if(i == str.length())
-            return 0;
-        if(dp[i] != -1)
-            return dp[i];
-        
-        int ans = 1000;
-        string temp = "";
-        for(int j = i; j < str.length(); j++)
-        {
-            temp.push_back(str[j]);
-            if(isPalindrome(temp))
-                ans = min(ans, 1 + solve(j+1, str, dp));
-        }
-        
-        return dp[i] = ans;
-    }
-
 public:
     int palindromicPartition(string str)
     {
-        vector<int> dp(str.length(), -1);
-        return solve(0, str, dp) - 1;
+        int n = str.length();
+        vector<int> dp(n+1, 0);
+        
+        for(int i = n-1; i >= 0; i--)
+        {
+            int ans = 1000;
+            for(int j = i; j < n; j++)
+            {
+                if(isPalindrome(str, i, j))
+                    ans = min(ans, 1 + dp[j+1]);
+            }
+            dp[i] = ans;
+        }
+        
+        return dp[0] - 1;   // substract 1 as, our code also do partition at End ie dp[n]
     }
 };
 
