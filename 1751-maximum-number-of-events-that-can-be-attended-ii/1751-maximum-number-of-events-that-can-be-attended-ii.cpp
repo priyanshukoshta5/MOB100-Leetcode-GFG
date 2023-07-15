2e3c5051e1812cde4d6b83b99febabac9f79c2dd
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-07-15 14:44:11 +0530

    Time: 480 ms (9.16%), Space: 69.2 MB (78.36%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,39 +1,41 @@
 class Solution {
-private:
-    int solve(int ind, int k, vector<vector<int>>& events, vector<int>& startDays, vector<vector<int>>& dp){
-        if(ind == events.size())
-            return 0;
-        if(dp[ind][k] != -1)
-            return dp[ind][k];
-
-        int notTake = 0 + solve(ind + 1, k, events, startDays, dp);
-
-        int start = events[ind][0];
-        int end = events[ind][1];
-        int value = events[ind][2];
-
-        int take = 0;
-        if(k == 1)
-            take = value;
-        else
-        {
-            int i = lower_bound(startDays.begin() + ind, startDays.end(), end+1) - startDays.begin();
-            int temp = value + solve(i, k-1, events, startDays, dp);
-            take = max(take, temp);
-        }
-
-        return dp[ind][k] = max(take, notTake);
-    }
-
 public:
     int maxValue(vector<vector<int>>& events, int k) {
+        int n = events.size();
+        
         sort(events.begin(), events.end());    //  sort increasing with START_DATE
         vector<int> startDays;
         for(auto e: events)
             startDays.push_back(e[0]);
 
-        vector<vector<int>> dp(events.size(), vector<int> (k+1, -1));
+        vector<vector<int>> dp(n+1, vector<int> (k+1, 0));
+        
+        for(int ind = n-1; ind >= 0; ind--)
+        {
+            for(int e = 1; e <= k; e++)
+            {
+                int notTake = 0 + dp[ind + 1][e];
 
-        return solve(0, k, events, startDays, dp);
+                int start = events[ind][0];
+                int end = events[ind][1];
+                int value = events[ind][2];
+
+                int take = 0;
+                if(e == 1)
+                    take = value;
+                else
+                {
+                    auto it = lower_bound(startDays.begin() + ind, startDays.end(), end+1);
+                    int i = it - startDays.begin(); 
+
+                    int temp = value + dp[i][e-1];
+                    take = max(take, temp);
+                }
+
+                dp[ind][e] = max(take, notTake);
+            }
+        }
+
+        return dp[0][k];
     }
 };
