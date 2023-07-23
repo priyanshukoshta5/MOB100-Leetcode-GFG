17ace48ce558c15ed66da932dd42efd5c7b69c52
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-07-23 13:50:10 +0530

    Time: 102 ms (67.13%), Space: 28 MB (67.98%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -10,27 +10,42 @@
  * };
  */
 class Solution {
-public:
-    vector<TreeNode*> allPossibleFBT(int n) {
-        if (n % 2 == 0) {
-            return {};
-        }
-
-        vector<vector<TreeNode*>> dp(n + 1);
-        dp[1].push_back(new TreeNode(0));
+private:
+    vector<TreeNode*> solve(int n, vector<vector<TreeNode*>> &dp){
+        if(dp[n].size() != 0)
+            return dp[n];
         
-        for (int count = 3; count <= n; count += 2) {
-            for (int i = 1; i < count - 1; i += 2) {
-                int j = count - 1 - i;
-                for (auto left : dp[i]) {
-                    for (auto right : dp[j]) {
-                        TreeNode* root = new TreeNode(0, left, right);
-                        dp[count].push_back(root);
-                    }
+        vector<TreeNode*> res;
+
+        int a = 1;
+        int b = n-2;
+        while(a < n)
+        {
+            vector<TreeNode*> leftTree = solve(a, dp);
+            vector<TreeNode*> rightTree = solve(b, dp);
+            for(TreeNode* l: leftTree)
+            {
+                for(TreeNode* r: rightTree)
+                {
+                    TreeNode* temp1 = new TreeNode(0, l, r);
+                    res.push_back(temp1);
                 }
             }
+
+            a += 2;
+            b -= 2;
         }
-        
-        return dp[n];
+
+        return dp[n] = res;
+    }
+
+public:
+    vector<TreeNode*> allPossibleFBT(int n) {
+        if(n%2 == 0)
+            return {};
+
+        vector<vector<TreeNode*>> dp(n+1);
+        dp[1].push_back(new TreeNode(0));
+        return solve(n, dp);
     }
 };
