be82a56ae296c66c20c6ff39592ed9305879091b
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-08-05 14:02:36 +0530

    Time: 2 ms (49.17%), Space: 6.1 MB (10.18%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1, -1);
+        dp[0] = 1;
+        return solve(n, dp);
+    }
+
+    int solve(int n, vector<int> &dp)
+    {
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        int cnt = 0;
+        for(int i = 1; i <= n; i++)     // choosing the position of the root in the values {1,2,3...}
+        {
+            int left = solve(i - 1, dp);
+            int right = solve(n - i, dp);
+            cnt = cnt + left * right;
+        }  
+
+        return dp[n] = cnt;
+    }
+};
