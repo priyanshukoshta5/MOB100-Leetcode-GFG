6e7fa6a948c095369c1bb0235e2e7d09eecea83a
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-08-13 13:45:46 +0530

    Time: 141 ms (77.54%), Space: 84 MB (90.46%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,42 +1,36 @@
 class Solution {
-private:
-    bool solve(int ind, vector<int>& nums, vector<int> &dp){
-        // reached the end of the array
-        if(ind == nums.size())  
-            return true;
-
-        // only 1 element left    
-        if(ind == nums.size() - 1)  
-            return false;
-
-        // if already calculated
-        if(dp[ind] != -1)
-            return dp[ind];
-
-        // to store the answer for the array from 'ind' to the end of array
-        int ans = false;
-
-        // condition 1: 2 equal elements
-        if(nums[ind] == nums[ind + 1])
-            ans = ans | solve(ind + 2, nums, dp);
-
-        if((ind + 2) < nums.size())
-        {
-            // condition 2: 3 equal elements
-            if(nums[ind] == nums[ind + 1] && nums[ind + 1] == nums[ind + 2])
-                ans = ans | solve(ind + 3, nums, dp);
-
-            // condition 3: 3 consecutive increasing elements
-            if((nums[ind + 1] - nums[ind]) == 1 && (nums[ind + 2] - nums[ind + 1]) == 1)
-                ans = ans | solve(ind + 3, nums, dp);
-        }
-
-        return dp[ind] = ans;
-    }
-
 public:
     bool validPartition(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return solve(0, nums, dp);
+        int n = nums.size();
+        
+        vector<bool> dp(n + 1, false);
+        
+        // base case
+        dp[n] = true;
+        
+        for(int ind = n - 2; ind >= 0; ind--)
+        {
+            // to store the answer for the array from 'ind' to the end of array
+            int ans = false;
+
+            // condition 1: 2 equal elements
+            if(nums[ind] == nums[ind + 1])
+                ans = ans | dp[ind + 2];
+
+            if((ind + 2) < n)
+            {
+                // condition 2: 3 equal elements
+                if(nums[ind] == nums[ind + 1] && nums[ind + 1] == nums[ind + 2])
+                    ans = ans | dp[ind + 3];
+
+                // condition 3: 3 consecutive increasing elements
+                if((nums[ind + 1] - nums[ind]) == 1 && (nums[ind + 2] - nums[ind + 1]) == 1)
+                    ans = ans | dp[ind + 3];
+            }
+
+            dp[ind] = ans;
+        }
+        
+        return dp[0];
     }
 };
