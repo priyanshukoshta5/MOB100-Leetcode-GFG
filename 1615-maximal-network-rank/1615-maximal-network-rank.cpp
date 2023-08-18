9d5efed5e161deed4e6f4fa31726c9cae6860d8b
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-08-18 14:01:34 +0530

    Time: 83 ms (69.81%), Space: 34 MB (49.21%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        // creating adjancy list
+        vector<vector<int>> adj(n);
+        for(auto e: roads)
+        {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+        for(int i = 0; i < n; i++)
+            sort(adj[i].begin(), adj[i].end());
+
+        // taking all pairs of nodes, and finding answer
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                int res = adj[i].size() + adj[j].size();    
+                // if there is a road between i and j
+                if(binary_search(adj[i].begin(), adj[i].end(), j))   
+                    res--;
+                ans = max(ans, res);
+            }
+        }
+
+        return ans;
+    }
+};
