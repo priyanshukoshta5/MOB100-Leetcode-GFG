d575d3a3c54d57bcd4cbec004fafd0ace77ee731
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-10-06 13:42:35 +0530

    Time: 2 ms (49.96%), Space: 6.4 MB (29.03%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        if(n == 2)
-            return 1;
-        if(n == 3)
-            return 2;
-        if(n == 4)
-            return 4;
-
-        int ans = 1;
-        while(n >= 3)
-        {
-            ans = ans * 3;
-            n -= 3;
-        }
-
-        if(n == 1)
-            return (ans / 3) * 4;
-        if(n == 2)
-            return ans * 2;
-        return ans;
+        if(n <= 3)
+            return n - 1;
+        
+        if(n % 3 == 0)
+            return pow(3, n / 3);
+        
+        if(n % 3 == 1)
+            return pow(3, n / 3 - 1) * 4;
+        
+        return pow(3, n / 3) * 2;
     }
 };
