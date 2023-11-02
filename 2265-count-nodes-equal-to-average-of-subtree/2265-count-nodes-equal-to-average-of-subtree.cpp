6db4767af442dc8ea4d853fc807621a642445004
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-11-02 17:43:34 +0530

    Time: 3 ms (92.80%), Space: 12.3 MB (54.24%) - LeetHub

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct Result{
+    int total, nodes, ansNodes;
+    Result(int t, int n, int a){
+        total = t;
+        nodes = n;
+        ansNodes = a;
+    }
+};
+
+class Solution {
+private:
+    Result dfs(TreeNode *node){
+        if(node == nullptr)
+            return Result(0, 0, 0);
+
+        Result left = dfs(node->left);
+        Result right = dfs(node->right);
+
+        Result cur = Result(left.total + right.total + node->val, 
+                            left.nodes + right.nodes + 1, 
+                            left.ansNodes + right.ansNodes);
+
+        if(cur.nodes != 0)
+            cur.ansNodes += (node->val == (cur.total / cur.nodes));
+        else
+            cur.ansNodes++;
+
+        return cur;
+    }
+
+public:
+    int averageOfSubtree(TreeNode* root) {
+        return dfs(root).ansNodes;
+    }
+};
