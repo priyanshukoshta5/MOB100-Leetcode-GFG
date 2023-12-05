eb4b095db7ed40fa156ac6b2ce9fbed8220d0f21
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-12-05 23:27:17 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (73.09%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        // return (n - 1);      most optimal, breaking logic
+        int ans = 0;
+        while(n > 1)
+        {
+            ans += n / 2;
+            if(n % 2)
+                n++;
+            n = n / 2;
+        }
+        return ans;
+    }
+};
