fa8b8b796a366ea9f275b1b23a39b3a0b062a2ab
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-12-06 15:36:34 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (70.53%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,18 +1,26 @@
 class Solution {
+private:
+    int getSumOfNNaturalNumbers(int n){ // for small value of N
+        return (n * (n + 1)) / 2;
+    }
+    int getSumOfAP(int start, int diff, int n){
+        return (n * (start + (n - 1) * diff)) / 2;
+    }
+    
 public:
     int totalMoney(int n) {
-        int sum1to7 = 1 + 2 + 3 + 4 + 5 + 6 + 7;
+        int complete_weeks = n / 7;
+        int remaining_days = n % 7;
         int total_money = 0;
-        int week = 0;
-        while(n >= 7)
-        {
-            total_money += (week * 7) + sum1to7;
-            week++;
-            n = n - 7;
-        }
-        total_money += (week * n);
-        for(int i = 1; i <= n; i++)
-            total_money += i;
+
+        // adding money in all the complete weeks 
+        total_money += complete_weeks *  getSumOfNNaturalNumbers(7);
+        total_money += getSumOfAP(0, 7, complete_weeks);
+
+        // adding money in days left
+        total_money += getSumOfNNaturalNumbers(remaining_days);
+        total_money += remaining_days * complete_weeks;
+
         return total_money;
     }
 };
