b7d5bf1ae01397469e28e88002e2eded1c8f8978
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-12-11 11:23:57 +0530

    Time: 8 ms (78.76%), Space: 12.7 MB (92.26%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,15 +1,21 @@
+// ~ Editorial, amazing
+
 class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
         unordered_map<int, int> freq;
         int n = arr.size();
         int _25 = n / 4;
-        for(int i: arr)
+        vector<int> possibilities = {arr[_25], arr[n / 2], arr[3 * n / 4]};
+        
+        for(int i: possibilities)
         {
-            freq[i]++;
-            if(freq[i] > _25)
+            int left = lower_bound(arr.begin(), arr.end(), i) - arr.begin();
+            int right = upper_bound(arr.begin(), arr.end(), i) - arr.begin();
+            if((right - left) > _25)
                 return i;
         }
+        
         return -1;
     }
 };
