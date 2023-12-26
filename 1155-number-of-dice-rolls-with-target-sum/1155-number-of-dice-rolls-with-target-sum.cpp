60a95394bf09d8bc383865b36be53727a771941f
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2023-12-26 11:45:39 +0530

    Time: 18 ms (85.87%), Space: 8.7 MB (65.29%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,28 +1,32 @@
+// O(target) space optimization with 2 arrays 
+
 class Solution {
 private:
     int MOD = 1e9+7;
 
 public:
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n + 1, vector<int> (target + 1, 0));
+        vector<int> prev(target + 1, 0);
         // base case
-        dp[0][0] = 1;
+        prev[0] = 1;
 
         long long res;
         for(int ind = 1; ind <= n; ind++)
         {
+            vector<int> cur(target + 1, 0);
             for(int tar = 1; tar <= target; tar++)
             {
                 res = 0;
                 for(int die = 1; die <= k && die <= tar; die++)
                 {
-                    res += dp[ind - 1][tar - die];
+                    res += prev[tar - die];
                     res %= MOD;
                 }
-                dp[ind][tar] = res;
+                cur[tar] = res;
             }
+            prev = cur;
         }
 
-        return dp[n][target];
+        return prev[target];
     }
 };
