c84d7f7821ff18b4fc823e38a654a34defa4d9d7
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2024-01-14 14:21:30 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find duplicate rows in a binary matrix/find-duplicate-rows-in-a-binary-matrix.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{   
+    public:
+    vector<int> repeatedRows(vector<vector<int>> &matrix, int M, int N) 
+    { 
+        vector<int> ans;
+        unordered_set<string> isPresent;
+        int i = 0;
+        for(auto r: matrix)
+        {
+            string temp;
+            for(int c: r)
+                temp.push_back('0' + c);
+            if(isPresent.find(temp) == isPresent.end())
+                isPresent.insert(temp);
+            else
+                ans.push_back(i);
+            i++;
+        }
+        return ans;
+    } 
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > matrix(row); 
+        for(int i=0; i<row; i++)
+        {
+            matrix[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        Solution ob;
+        vector<int> ans = ob.repeatedRows(matrix, row, col);
+
+
+        for (int i = 0; i < ans.size(); ++i)
+        {
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
