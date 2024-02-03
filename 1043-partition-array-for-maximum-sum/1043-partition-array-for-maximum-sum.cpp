33fad7391f048ce7343eb86a8d9dd7178da87b9f
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2024-02-03 15:25:50 +0530

    Time: 5 ms (95.89%), Space: 10.8 MB (26.98%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,27 +1,21 @@
 class Solution {
-private:
-    int solve(int i, int j, int curMax, vector<int> &arr, int k, vector<vector<int>> &dp){
-        int curLen = j - i;
-
-        if(j == arr.size())
-            return (curLen * curMax);
-        if(dp[i][j] != -1)
-            return dp[i][j];
-
-        // starting a new subarray
-        int notTake = (curLen * curMax) + solve(j, j + 1, arr[j], arr, k, dp);  
-
-        // continue current subarray
-        int take = 0;
-        if(curLen < k)
-            take = solve(i, j + 1, max(curMax, arr[j]), arr, k, dp);
-
-        return dp[i][j] = max(notTake, take);
-    }
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1)); 
-        return solve(0, 1, arr[0], arr, k, dp);
+        
+        vector<int> dp(n + 1, 0);
+        dp[n] = 0; // base case;
+        
+        for(int i = n - 1; i >= 0; i--)
+        {
+            int curMax = arr[i];
+            for(int j = i; j < n && j < (i + k); j++)
+            {
+                curMax = max(curMax, arr[j]);
+                dp[i] = max(dp[i], (curMax * (j - i + 1)) + dp[j + 1]);
+            }
+        }
+        
+        return dp[0];
     }
 };
