507098026f72f33fd3efe6278b638ea4e75ef59f
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2024-03-13 11:17:05 +0530

    Time: 4 ms (35.26%), Space: 7.3 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,18 +1,28 @@
+// class Solution {
+// public:
+//     int pivotInteger(int n) {
+//         int left = 0;
+//         int right = (n * (n + 1)) / 2;
+//         int i = 1;
+//         while(left < right)
+//         {
+//             left += i;
+//             // cout<<i<<" "<<left<<" "<<right<<endl;
+//             if(left == right)
+//                 return i;
+//             right -= i;
+//             i++;
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
-public:
-    int pivotInteger(int n) {
-        int left = 0;
-        int right = (n * (n + 1)) / 2;
-        int i = 1;
-        while(left < right)
-        {
-            left += i;
-            // cout<<i<<" "<<left<<" "<<right<<endl;
-            if(left == right)
-                return i;
-            right -= i;
-            i++;
-        }
-        return -1;
-    }
+ public:
+  int pivotInteger(int n) {
+        const int sum = (n * (n + 1) / 2);
+        const int pivot = sqrt(sum);
+        // If pivot * pivot is equal to sum (pivot found) return pivot, else return -1
+        return pivot * pivot == sum ? pivot : -1;
+  }
 };
