3e90f8027f00672e0139468d8a1257b54deddcde
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2024-04-22 09:43:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Row with minimum number of 1's/row-with-minimum-number-of-1s.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minRow(int n, int m, vector<vector<int>> a) {
+        int min1 = m, ansRow = 1;
+        for(int r = 0; r < n; r++)
+        {
+            int one = count(a[r].begin(), a[r].end(), 1);
+            if(one < min1)
+            {
+                min1 = one;
+                ansRow = r + 1;
+            }
+        }
+        return ansRow;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin >> N >> M;
+        vector<vector<int>> A(N, vector<int>(M));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                cin >> A[i][j];
+            }
+        }
+        Solution ob;
+        cout << ob.minRow(N, M, A) << "\n";
+    }
+}
+// } Driver Code Ends
