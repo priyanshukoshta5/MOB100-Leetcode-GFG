93745dfefc4b06acba6f52ff8afbe4836cb76dc6
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2024-06-08 14:22:08 +0530

    Time: 166 ms (87.75%), Space: 134.6 MB (59.36%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,26 @@
+// With the help of Editorial
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int prefixMod = 0;
+        unordered_map<int, int> modSeen;
+        modSeen[0] = -1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            prefixMod = (prefixMod + nums[i]) % k;
+
+            if (modSeen.find(prefixMod) != modSeen.end()) {
+                // ensures that the size of subarray is atleast 2
+                if (i - modSeen[prefixMod] > 1) 
+                    return true;
+                
+            } else {
+                // mark the value of prefixMod with the current index.
+                modSeen[prefixMod] = i;
+            }
+        }
+
+        return false;
+    }
+};
