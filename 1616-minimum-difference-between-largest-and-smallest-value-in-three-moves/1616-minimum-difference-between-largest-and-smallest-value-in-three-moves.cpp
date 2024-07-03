0564e862b8464cab87068d2a27348f6c3196da82
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2024-07-03 14:32:49 +0530

    Time: 81 ms (9.83%), Space: 38.9 MB (15.52%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -5,14 +5,35 @@ public:
         if(n <= 4)
             return 0;
         
-        sort(nums.begin(), nums.end());
+        priority_queue<int> minH;
+        priority_queue<int, vector<int>, greater<int>> maxH;
+        for(int i: nums)
+        {
+            minH.push(i);
+            maxH.push(i);
+            if(minH.size() > 4)
+            {
+                minH.pop();
+                maxH.pop();
+            }
+        }
+
+        vector<int> small, large;
+        while(!minH.empty())
+        {
+            small.push_back(minH.top()); minH.pop();
+            large.push_back(maxH.top()); maxH.pop();
+        }
+
+        // for(int i = 0; i < 4; i++)
+        //     cout<<large[i]<<" "<<small[3 - i]<<endl;
 
         int ans = INT_MAX;
-        
-        ans = min(ans, nums[n - 1] - nums[3]);    // removed 3 left, 0 right
-        ans = min(ans, nums[n - 2] - nums[2]);    // removed 2 left, 1 right
-        ans = min(ans, nums[n - 3] - nums[1]);    // removed 1 left, 2 right
-        ans = min(ans, nums[n - 4] - nums[0]);    // removed 0 left, 3 right
+
+        ans = min(ans, large[0] - small[3]);    // removed 3 left, 0 right
+        ans = min(ans, large[1] - small[2]);    // removed 2 left, 1 right
+        ans = min(ans, large[2] - small[1]);    // removed 1 left, 2 right
+        ans = min(ans, large[3] - small[0]);    // removed 0 left, 3 right
 
         return ans;
     }
