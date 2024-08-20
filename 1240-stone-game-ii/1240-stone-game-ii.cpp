5801104dc38af8ddb8dc9823e9e4f82c10dec99f
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2024-08-20 16:22:23 +0530

    Time: 7 ms (91.62%), Space: 12.2 MB (66.14%) - LeetHub

---
+++ 1240-stone-game-ii/1240-stone-game-ii.cpp
@@ -0,0 +1,32 @@
+// Not my solution
+
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        
+        vector<vector<int>> dp(n, vector<int> (n, 0));
+        vector<int> sum(n+1, 0);
+        
+        for(int i = n-1; i >= 0; i--) 
+            sum[i] = sum[i+1] + piles[i];
+        
+        return helper(dp, 1, 0, sum);
+    }
+    
+    int helper(vector<vector<int>>& dp, int M, int pos, vector<int>& sum) {
+        if((pos + 2*M) >= (sum.size()-1)) 
+            return sum[pos]; 
+        
+        int res = 0;
+        for(int i = 1; i <= 2*M; i++) 
+        {
+            int s = sum[pos] - sum[i+pos];
+            if(dp[i+pos][max(i, M)] == 0) 
+                dp[i+pos][max(i, M)] = helper(dp, max(i, M), i + pos, sum);
+            res = max(res, s + sum[i+pos] - dp[i+pos][max(i, M)]);
+        }
+        
+        return res;
+    }
+};
