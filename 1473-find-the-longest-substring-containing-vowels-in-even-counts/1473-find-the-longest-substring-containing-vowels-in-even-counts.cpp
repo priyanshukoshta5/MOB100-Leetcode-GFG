e6eaf511497c6bada93b9a54342c2db13d4bec8d
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2024-09-15 16:59:20 +0530

    Time: 42 ms (73.26%), Space: 21.5 MB (6.74%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,39 +1,31 @@
 class Solution {
 private:
-    void flipbit(string &mask, int i){
-        if(mask[i] == '1')
-            mask[i] = '0';
-        else 
-            mask[i] = '1';
-        return;
-    }
-
     bool isVowel(char ch){
         return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
 
 public:
     int findTheLongestSubstring(string s) {
-        string mask = "00000";
         map<char, int> code = {{'a', 0}, {'e', 1}, {'i', 2}, {'o', 3}, {'u', 4}};
+
+        int mask = 00000;
         int ans = 0;
-        map<string, int> mp;
+        vector<int> mp(10001, -2);
         mp[mask] = -1;
+
         for(int i = 0; i < s.length(); i++)
         {
             if(isVowel(s[i]))
-                flipbit(mask, code[s[i]]);
+                mask = mask ^ (1 << code[s[i]]);
 
-            if(mp.find(mask) == mp.end())
+            if(mp[mask] == -2)
                 mp[mask] = i;
             else
                 ans = max(ans, i - mp[mask]);
 
-            // cout<<i<<" "<<mask<<" "<<ans<<endl;
+            // cout<<i<<" "<<mask<<" "<<ans<<" "<<mp[mask]<<endl;
         }
-
-        // for(auto x: mp)
-        //     cout<<x.first<<" "<<x.second<<endl;
+        
         return ans;
     }
 };
