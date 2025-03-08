12b403cac26435521c03b2ae6d4af956e138a3e3
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2025-03-08 16:32:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string longestPalindrome(string &s) {
+        // code here
+        string ans;
+        ans.push_back(s[0]);
+        int i = 0, start, end, len;
+        
+        while(i < s.length())
+        {
+            start = i-1;
+            end = i+1;
+            
+            // ODD Length
+            while(start >= 0 && end < s.length() && s[start] == s[end])
+            {
+                start--;
+                end++;
+            }
+            len = end - start - 1;
+            if(ans.length() < len)
+                ans = s.substr(start+1, len);
+            
+            
+            if((i+1) == s.length())
+                break;
+                
+            // EVEN Length
+            start = i;
+            end = i+1;
+            while(start >= 0 && end < s.length() && s[start] == s[end])
+            {
+                start--;
+                end++;
+            }
+            len = end - start - 1;
+            if(ans.length() < len)
+                ans = s.substr(start+1, len);
+            
+            i++;
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+
+        Solution ob;
+        cout << ob.longestPalindrome(S) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+}
+
+// } Driver Code Ends
