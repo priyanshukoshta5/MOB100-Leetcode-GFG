a621bf3c35745ce8b6bb3be46a9bf8a633b5499f
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2025-07-29 00:55:22 +0530

    Time: 8 ms (80.56%), Space: 10.5 MB (86.47%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,31 +1,21 @@
-// ~Editorial
-
 class Solution {
-public:
-    int countMaxOrSubsets(vector<int>& nums) {
-        int maxOrValue = 0;
-        for (int num : nums) {
-            maxOrValue |= num;
-        }
-        return countSubsets(nums, 0, 0, maxOrValue);
+private:
+    int runMyFunctionRun(int ind, vector<int> &nums, int curVal, int maxOR){
+        if(ind == nums.size())
+            return curVal == maxOR;
+
+        int take = runMyFunctionRun(ind + 1, nums, curVal | nums[ind], maxOR);
+        int notTake = runMyFunctionRun(ind + 1, nums, curVal, maxOR);
+
+        return take + notTake;
     }
 
-private:
-    int countSubsets(vector<int>& nums, int index, int currentOr,
-                     int targetOr) {
-        // Base case: reached the end of the array
-        if (index == nums.size()) {
-            return (currentOr == targetOr) ? 1 : 0;
-        }
+public:
+    int countMaxOrSubsets(vector<int>& nums) {
+        int maxOR = 0;
+        for(int i: nums)
+            maxOR = maxOR | i;
 
-        // Don't include the current number
-        int countWithout = countSubsets(nums, index + 1, currentOr, targetOr);
-
-        // Include the current number
-        int countWith =
-            countSubsets(nums, index + 1, currentOr | nums[index], targetOr);
-
-        // Return the sum of both cases
-        return countWithout + countWith;
+        return runMyFunctionRun(0, nums, 0, maxOR);
     }
 };
