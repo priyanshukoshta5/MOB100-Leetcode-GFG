ba9f7e4a48d63f86b47e025071035f1963c8edcf
Priyanshu Koshta <113628738+priyanshukoshta5@users.noreply.github.com>
2025-09-02 01:17:45 +0530

    Time: 367 ms (29.85%), Space: 109 MB (18.84%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -1,31 +1,41 @@
-// ~Solutions
-
 class Solution {
- public:
-  double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-    priority_queue<tuple<double, int, int>> maxHeap;
-    for (const vector<int>& c : classes) {
-      const int pass = c[0];
-      const int total = c[1];
-      maxHeap.emplace(extraPassRatio(pass, total), pass, total);
-    }
-    for (int i = 0; i < extraStudents; ++i) {
-      const auto [_, pass, total] = maxHeap.top();
-      maxHeap.pop();
-      maxHeap.emplace(extraPassRatio(pass + 1, total + 1), pass + 1, total + 1);
-    }
-    double ratioSum = 0;
-    while (!maxHeap.empty()) {
-      const auto [_, pass, total] = maxHeap.top();
-      maxHeap.pop();
-      ratioSum += pass / static_cast<double>(total);
-    }
-    return ratioSum / classes.size();
-  }
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        priority_queue<pair<double, pair<int, int>>> maxH;
 
- private:
-  double extraPassRatio(int pass, int total) {
-    return (pass + 1) / static_cast<double>(total + 1) -
-           pass / static_cast<double>(total);
-  }
+        for(vector<int> cls: classes){
+            int pass = cls[0];
+            int total = cls[1];
+            double passRatio = pass / (1.0 * total);
+            double newPassRatio = (pass + 1) / (1.0 * (total + 1));
+            maxH.push({newPassRatio - passRatio, {pass, total}});
+        }
+
+        while(extraStudents--){
+            auto top = maxH.top();
+            maxH.pop();
+            // cout<<top.first<<" "<<top.second.first<<" "<<top.second.second<<endl;
+            int pass = top.second.first + 1;
+            int total = top.second.second + 1;
+            double passRatio = pass / (1.0 * total);
+            double newPassRatio = (pass + 1) / (1.0 * (total + 1));
+
+            maxH.push({newPassRatio - passRatio, {pass, total}});
+        }
+
+        double ans = 0;
+        while(!maxH.empty()){
+            auto top = maxH.top();
+            int pass = top.second.first;
+            int total = top.second.second;
+            double passRatio = pass / (1.0 * total);
+            ans += passRatio;
+            // cout<<top.first<<" "<<top.second.first<<" "<<top.second.second<<endl;
+            maxH.pop();
+        }
+
+        ans = ans / classes.size();
+
+        return ans;
+    }
 };
